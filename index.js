671aee48b9d4d22c1349cbeb81c9a79de8ec5d8d
rohit <rohit.chandekar786@gmail.com>
2021-05-26 16:18:45 +0530

    set up view engine

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use('/', require('./routes'));
 
-
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
