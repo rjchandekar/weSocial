95506e8e13222e91123f4e94a7ace15a236f8222
rohit <rohit.chandekar786@gmail.com>
2021-06-01 21:47:35 +0530

    Deleting posts and comments

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const postController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 
 
 
