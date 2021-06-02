9c2fd0ef2b78f2fafe968678634ed726be1ea3d8
rohit <rohit.chandekar786@gmail.com>
2021-06-02 13:05:31 +0530

    Converting code to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,28 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
 
-    // Post.find({}, function(err, post){
-    //     return res.render('home', {
-    //         title: "WeSocial | Home",
-    //         posts: posts
-    //     });
-    // });
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "WeSocial | Home",
-                posts:  posts,
-                all_users: users
-            });
-        })
+    try {
+        //Populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
 
-        
-    })
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title: "WeSocial | Home",
+            posts:  posts,
+            all_users: users
+        });
+    } catch (err) {
+        console.log('Error', err);
+    }
 
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 
+
+//lets keep it same as before
 module.exports.profile = function(req,res){
     User.findById(req.params.id, function(err, user){
         return res.render('user_profile',{
