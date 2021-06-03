0ba27820a58fa457448cd18746f28dcc10dde6b9
rohit <rohit.chandekar786@gmail.com>
2021-06-03 14:14:08 +0530

    sending template emails using nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -10,7 +10,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'nu104075@gmail.com',
-        pass: '040999$Rohit'
+        pass: 'Password'
     }
 });
 
